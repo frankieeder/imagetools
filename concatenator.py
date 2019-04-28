7159761970351e6c14486bceeef23bc7871b0a2c
Frankie Eder <frankieeder@frankies-imac.lan>
2019-04-28 15:22:55 -0700

    Made an in-memory, faster concatenator.

--- concatenator.py
+++ concatenator.py
@@ -3,28 +3,26 @@ import numpy as np
 import os
 import cv2
 import time
+from itertools import islice
 import argparse
 
 class Concatenator(ImageStepper):
-    SOURCE_VIDEO = 1
-    SOURCE_FOLDER = 0
     def __init__(self,
                  source,
-                 concat_length=-1,  # TODO: Implement input length
-                 resize=False, out_w=1920, out_h=1080,  # TODO: Implement output resizing
+                 concat_length=None,
+                 resize=False, out_w=1920, out_h=1080,
                  region=SliceMaker()[:,1],
                  print_status=False,
                  interpolation=cv2.INTER_NEAREST
                  ):
         self.source = source
-        self.concat_length = concat_length if concat_length > 0 else len(source)
+        self.concat_length = concat_length if concat_length else len(source)
         self.resize = resize
         self.out_w = out_w
         self.out_h = out_h
         self.region = region
         self.print_status = print_status
         self.interpolation = interpolation
-        # TODO: Could implement an iterator for file inputs to concatenator
 
     def __next__(self):
         if self.print_status:
@@ -53,17 +51,55 @@ class Concatenator(ImageStepper):
         return self
 
 
+class CubeConcatenator:
+    def __init__(self,
+                 source,
+                 concat_length=None,
+                 resize=False, out_w=1920, out_h=1080,
+                 print_status=False,
+                 interpolation=cv2.INTER_NEAREST
+                 ):
+        self.source = source
+        self.concat_length = concat_length if concat_length else len(source)
+        self.resize = resize
+        self.out_w = out_w
+        self.out_h = out_h
+        self.print_status = print_status
+        self.interpolation = interpolation
+        datas = []
+        for c, im in enumerate(islice(self.source, concat_length)):
+            datas.append(np.expand_dims(im, axis=0))
+            if print_status:
+                print(f"Loading image {c} / {self.concat_length}")
+        data = np.concatenate(datas, axis=0)
+        data = np.swapaxes(data, 0, 2)
+        self.data = data
+        self.iterator = (im for im in self.data)
+
+    def __next__(self):
+        final = next(self.iterator)
+        if self.resize:
+            final = cv2.resize(
+                src=final,
+                dsize=(self.out_w, self.out_h),
+                interpolation=self.interpolation
+            )
+        return final
+
+    def __iter__(self):
+        return self
+
 if __name__ == "__main__":
-    images = SimpleFolderIterator("/Volumes/My Passport for Mac/Pictures/Design/PROJECTS/55 (Concatenations II)/Video Approach/video jpegs/jonah")
-    concat = Concatenator(
+    images = SimpleFolderIterator(
+        "/Volumes/My Passport for Mac/Pictures/Design/PROJECTS/55 (Concatenations II)/Video Approach/video jpegs/jonah")
+    concat = CubeConcatenator(
         source=images,
         print_status=True,
-        region=SliceMaker()[:,960],
         resize=True
     )
-    
-    for c, im in enumerate(concat):
-        cv2.imwrite(f"./jonah_{c}.jpg", im)
+    for c in range(concat.concat_length):
+        print(f"Saving image {c}")
+        cv2.imwrite(f"./jonah_{c}.jpg", next(concat))
 
 
 #TODO: Implement command line args
